d2afc801a34bb9c7f31e118f337b895a58fa4bf4
benjGam <mecopilive@gmail.com>
2024-09-18 12:53:47 +0200

    test: add test to check if a sentence is in snake_case

--- test/test.ts
+++ test/test.ts
@@ -8,10 +8,14 @@ describe("Casing functions test", () => {
 
   test('Should be equal to "camelCase"', () => {
     expect(StringFormatter.resolveCase("myTestIsBetter")).toBe("camelCase");
-  })
+  });
 
   test('Should be equal to "PascalCase"', () => {
     expect(StringFormatter.resolveCase("MyTestIsBetter")).toBe("pascalCase");
-  })
+  });
+
+  test('Should be equal to "snake_case"', () => {
+    expect(StringFormatter.resolveCase("my_test_is_better")).toBe("snakeCase");
+  });
 
 });
