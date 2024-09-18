6f43ed3ddec01f40fb5a2438a1fc5b1673531486
benjGam <mecopilive@gmail.com>
2024-09-18 12:50:38 +0200

    test: add test to check if a sentence is in camelCase

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,13 @@
-import { StringFormatter } from "../src";
+import { Casing, StringFormatter } from "../src";
 
 test('Should return `This is a test`', () => {
   expect(StringFormatter.normalizeSpaces("This   is    a  test")).toBe("This is a test");
+});
+
+describe("Casing functions test", () => {
+
+  test('Should be equal to "camelCase"', () => {
+    expect(StringFormatter.resolveCase("myTestIsBetter")).toBe("camelCase");
+  })
+
 });
