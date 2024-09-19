a2a3bdfee6140414cc9eb12f8ee972bef2ca1034
benjGam <mecopilive@gmail.com>
2024-09-19 12:24:47 +0200

    feat: add base of package

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export enum Case {
+  CAMEL_CASE = 'camelCase',
+  PASCAL_CASE = 'PascalCase',
+  SNAKE_CASE = 'snake_case',
+}
+
+export class StringUtils {
+  /**
+   *
+   *
+   */
+
+  public static determineCase(str: string): Case | undefined {
+    return undefined;
+  }
+}
