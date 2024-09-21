43acd5396c6e09d9290f8d643201ee2d79d89ae7
benjGam <mecopilive@gmail.com>
2024-09-21 09:41:30 +0200

    chore: use camelCase to name knownCases instead of Pascal one

--- src/case.ts
+++ src/case.ts
@@ -6,18 +6,18 @@ export interface ICase {
 
 export const knownCases: ICase[] = [
   {
-    name: 'SnakeCase',
+    name: 'snakeCase',
     matcher: /[a-zA-Z]+(?:_[a-zA-Z]+)*/,
     splitter: /_/,
   },
   {
-    name: 'PascalCase',
+    name: 'pascalCase',
     matcher:
       /[A-Z]([A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*/,
     splitter: /([A-Z]+[a-z]*)/,
   },
   {
-    name: 'CamelCase',
+    name: 'camelCase',
     matcher: /[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?/,
     splitter: /([A-Z]+[a-z]*)/,
   },
