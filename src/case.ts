4a6d028017b9d4120abfefff06a13df66d0aabe6
benjGam <mecopilive@gmail.com>
2024-09-21 10:11:32 +0200

    docs: add TSDoc for ICase interface

--- src/case.ts
+++ src/case.ts
@@ -1,3 +1,13 @@
+/**
+ * This interface provide a structure for literal objects
+ * It brings reliable way to add unmanaged cases without any other code writing
+ *
+ * @interface ICase
+ * @field {string} name is used to represent a matcher & splitter for case
+ * @field {RegExp} matcher is used with Regex operations to check if a given string match
+ * @field {RegExp | string} splitter is used to split given string who match `matcher`
+ *
+ */
 export interface ICase {
   name: string;
   matcher: RegExp;
