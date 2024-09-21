c4cb8de7c7975e7b017090c92aa430d6c9a6979e
benjGam <mecopilive@gmail.com>
2024-09-21 09:06:10 +0200

    docs: add TSDoc for 'determineCase' method

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ export enum Case {
 
 export class StringUtils {
   /**
+   * Returns the case with which parameters was written.
    *
+   * @param str - The string to test to determine case
    *
+   * @returns The case of parameter
    */
-
   public static determineCase(str: string): Case | undefined {
     return undefined;
   }
