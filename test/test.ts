3a91a78c2b8639133c027a8ac376fb9a1bec1cc3
benjGam <mecopilive@gmail.com>
2024-09-21 09:40:49 +0200

    fix: do not use 'Case' enum anymore

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,10 @@
-import { Case, StringUtils } from '../src';
+import { StringUtils } from '../src';
 
 describe('Determine case of given string.', () => {
-  const samples = new Map<Case, string>([
-    [Case.CAMEL_CASE, 'thisIsATest'],
-    [Case.PASCAL_CASE, 'ThisIsATest'],
-    [Case.SNAKE_CASE, 'this_is_a_test'],
+  const samples = new Map<string, string>([
+    ['camelCase', 'thisIsATest'],
+    ['pascalCase', 'ThisIsATest'],
+    ['snakeCase', 'this_is_a_test'],
   ]);
 
   for (const key of samples.keys()) {
