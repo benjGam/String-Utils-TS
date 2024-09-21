181835490b3fa733f1a6c0aff1e5077562cc7df4
benjGam <mecopilive@gmail.com>
2024-09-21 09:39:08 +0200

    feat: implement table of ICase objects

--- src/case.ts
+++ src/case.ts
@@ -3,3 +3,22 @@ export interface ICase {
   matcher: RegExp;
   splitter: RegExp | string;
 }
+
+export const knownCases: ICase[] = [
+  {
+    name: 'SnakeCase',
+    matcher: /[a-zA-Z]+(?:_[a-zA-Z]+)*/,
+    splitter: /_/,
+  },
+  {
+    name: 'PascalCase',
+    matcher:
+      /[A-Z]([A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*/,
+    splitter: /([A-Z]+[a-z]*)/,
+  },
+  {
+    name: 'CamelCase',
+    matcher: /[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?/,
+    splitter: /([A-Z]+[a-z]*)/,
+  },
+];
