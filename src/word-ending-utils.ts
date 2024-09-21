208c7b45b2a668b54577a878b2ff9e5100ed9d31
benjGam <mecopilive@gmail.com>
2024-09-21 14:40:13 +0200

    feat: implement 'IWordEnding' interface

---
+++ src/word-ending-utils.ts
@@ -0,0 +1,40 @@
+/**
+ * This interface provide a structure to register word endings forms
+ *
+ * @interface IWordEnding
+ * @field {string} pluralForm is used to store a plural form of ending
+ * @field {string} singularForm is used to store the singular form of the plural form ending
+ *
+ * @example
+ * ```
+ * pluralForm: 'ies'
+ * singularForm: 'y'
+ * ```
+ */
+export interface IWordEnding {
+  pluralForm: string;
+  singularForm: string;
+}
+
+/**
+ * This object is used to list plural and singular forms
+ * of words.
+ */
+export const wordEndings: IWordEnding[] = [
+  {
+    pluralForm: 'sses',
+    singularForm: 'ss',
+  },
+  {
+    pluralForm: 'ies',
+    singularForm: 'y',
+  },
+  {
+    pluralForm: 'es',
+    singularForm: 'e',
+  },
+  {
+    pluralForm: 's',
+    singularForm: '',
+  },
+];
