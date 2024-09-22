0fee38f55c70fdf26675b7f5de7f31055651e945
benjGam <mecopilive@gmail.com>
2024-09-22 12:25:24 +0200

    refactor(words): split tests into suits

--- test/words.spec.ts
+++ test/words.spec.ts
@@ -1,74 +1,6 @@
 import StringUtilsWord, { IWordEnding } from '../src/word-ending-utils';
 
-describe('Plural and Singular operations', () => {
-  const isPluralReturns = new Map<string, boolean>([
-    ['Pass', false],
-    ['Passes', true],
-    ['Category', false],
-    ['Categories', true],
-    ['Bees', true],
-    ['Bee', false],
-    ['Cars', true],
-    ['Bet', false],
-  ]);
-
-  for (const key of isPluralReturns.keys()) {
-    test(`Should return '${isPluralReturns.get(key)!}' for word = '${key}'`, () => {
-      expect(StringUtilsWord.isPlural(key)).toBe(isPluralReturns.get(key)!);
-    });
-  }
-
-  const isSingularReturns = new Map<string, boolean>(
-    Array.from(isPluralReturns.keys()).map((key) => [
-      key,
-      !isPluralReturns.get(key),
-    ]),
-  );
-
-  for (const key of isSingularReturns.keys()) {
-    test(`Should return '${isSingularReturns.get(key)!}' for word = '${key}'`, () => {
-      expect(StringUtilsWord.isSingular(key)).toBe(isSingularReturns.get(key)!);
-    });
-  }
-
-  const pluralizeWords = new Map<string, string>([
-    ['Pass', 'Passes'],
-    ['Category', 'Categories'],
-    ['Car', 'Cars'],
-    ['Bee', 'Bees'],
-    ['', ''],
-    ['List', 'Lists'],
-    ['Lists', 'Lists'],
-    ['C', 'C'],
-    ['Of', 'Ofs'],
-    ['      ', '      '],
-  ]);
-
-  for (const key of pluralizeWords.keys()) {
-    test(`Should return '${pluralizeWords.get(key)!}' for word = '${key}'`, () => {
-      expect(StringUtilsWord.pluralize(key)).toBe(pluralizeWords.get(key)!);
-    });
-  }
-
-  const singularizeWords = new Map<string, string>([
-    ['Passes', 'Pass'],
-    ['Categories', 'Category'],
-    ['Cars', 'Car'],
-    ['Bees', 'Bee'],
-    ['', ''],
-    ['Lists', 'List'],
-    ['List', 'List'],
-    ['C', 'C'],
-    ['Ofs', 'Of'],
-    ['      ', '      '],
-  ]);
-
-  for (const key of singularizeWords.keys()) {
-    test(`Should return '${singularizeWords.get(key)!}' for word = '${key}'`, () => {
-      expect(StringUtilsWord.singularize(key)).toBe(singularizeWords.get(key)!);
-    });
-  }
-
+describe('Get word ending', () => {
   const getWordEndingReturns = new Map<string, string>([
     ['Pass', 'ss'],
     ['Passes', 'sses'],
@@ -152,6 +84,78 @@ describe('Plural and Singular operations', () => {
       expect(StringUtilsWord.getCorrespondingEnding(key)).toEqual(
         getCorrespondingEndingReturns.get(key)!,
       );
+    });
+  }
+});
+
+describe('Is singular or plural', () => {
+  const isPluralReturns = new Map<string, boolean>([
+    ['Pass', false],
+    ['Passes', true],
+    ['Category', false],
+    ['Categories', true],
+    ['Bees', true],
+    ['Bee', false],
+    ['Cars', true],
+    ['Bet', false],
+  ]);
+
+  for (const key of isPluralReturns.keys()) {
+    test(`Should return '${isPluralReturns.get(key)!}' for word = '${key}'`, () => {
+      expect(StringUtilsWord.isPlural(key)).toBe(isPluralReturns.get(key)!);
+    });
+  }
+
+  const isSingularReturns = new Map<string, boolean>(
+    Array.from(isPluralReturns.keys()).map((key) => [
+      key,
+      !isPluralReturns.get(key),
+    ]),
+  );
+
+  for (const key of isSingularReturns.keys()) {
+    test(`Should return '${isSingularReturns.get(key)!}' for word = '${key}'`, () => {
+      expect(StringUtilsWord.isSingular(key)).toBe(isSingularReturns.get(key)!);
+    });
+  }
+});
+
+describe('Move from singular to plural and vice-versa', () => {
+  const pluralizeWords = new Map<string, string>([
+    ['Pass', 'Passes'],
+    ['Category', 'Categories'],
+    ['Car', 'Cars'],
+    ['Bee', 'Bees'],
+    ['', ''],
+    ['List', 'Lists'],
+    ['Lists', 'Lists'],
+    ['C', 'C'],
+    ['Of', 'Ofs'],
+    ['      ', '      '],
+  ]);
+
+  for (const key of pluralizeWords.keys()) {
+    test(`Should return '${pluralizeWords.get(key)!}' for word = '${key}'`, () => {
+      expect(StringUtilsWord.pluralize(key)).toBe(pluralizeWords.get(key)!);
+    });
+  }
+
+  const singularizeWords = new Map<string, string>([
+    ['Passes', 'Pass'],
+    ['Categories', 'Category'],
+    ['Cars', 'Car'],
+    ['Bees', 'Bee'],
+    ['', ''],
+    ['Lists', 'List'],
+    ['List', 'List'],
+    ['C', 'C'],
+    ['Ofs', 'Of'],
+    ['      ', '      '],
+  ]);
+
+  for (const key of singularizeWords.keys()) {
+    test(`Should return '${singularizeWords.get(key)!}' for word = '${key}'`, () => {
+      expect(StringUtilsWord.singularize(key)).toBe(singularizeWords.get(key)!);
     });
   }
 });
