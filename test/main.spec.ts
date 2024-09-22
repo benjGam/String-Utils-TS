b3f6616a11842000e552d977510d4c9e25b5303b
benjGam <mecopilive@gmail.com>
2024-09-22 12:55:36 +0200

    chore: move tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,21 +1,21 @@
 import { StringUtils } from '../src/main';
 
-describe('Non related features', () => {
-  const isBlankExpectedReturns = new Map<string, boolean>([
-    ['hey', false],
-    ['  hey  ', false],
-    ['     ', true],
-    ['', true],
-  ]);
+const isBlankExpectedReturns = new Map<string, boolean>([
+  ['hey', false],
+  ['  hey  ', false],
+  ['     ', true],
+  ['', true],
+]);
 
-  for (const strKey of isBlankExpectedReturns.keys()) {
-    test(`Should return '${isBlankExpectedReturns.get(strKey)!}' for str = '${strKey}'`, () => {
-      expect(StringUtils.isBlank(strKey)).toBe(
-        isBlankExpectedReturns.get(strKey)!,
-      );
-    });
-  }
+for (const strKey of isBlankExpectedReturns.keys()) {
+  test(`Should return '${isBlankExpectedReturns.get(strKey)!}' for str = '${strKey}'`, () => {
+    expect(StringUtils.isBlank(strKey)).toBe(
+      isBlankExpectedReturns.get(strKey)!,
+    );
+  });
+}
 
+describe('isUpper & isLower tests', () => {
   const isUpperReturns = new Map<string, boolean>([
     ['H', true],
     ['h', false],
