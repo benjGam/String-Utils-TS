15a892b11693f6add2314f460b38e56d795ed2ee
benjGam <mecopilive@gmail.com>
2024-09-25 00:52:01 +0200

    fix: rework class to be usable in that context

--- test/test.utils.ts
+++ test/test.utils.ts
@@ -1,14 +1,31 @@
-export default class JestUtils {
-  public static runBasicTests(
-    expectedReturns: Map<any, any>,
-    functionToTest: Function,
-  ): void {
+interface Type<T = any> extends Function {
+  new (...args: any[]): T;
+}
+
+export default class JestRunner {
+  private _classToInvoke: Type;
+
+  constructor(classToInvoke: Type) {
+    this._classToInvoke = classToInvoke;
+  }
+
+  public runBasicTests(expectedReturns: Map<any, any>, fn: Function): void {
+    this.checkInvokation(fn);
+
     for (const [input, output] of expectedReturns.entries()) {
-      test(`[${functionToTest.name}] Should return '${expectedReturns}'`, () => {
-        expect(functionToTest(input))[
+      test(`[${fn.name}] Should return '${output}'`, () => {
+        expect(this._classToInvoke[fn.name](input))[
           typeof output === 'object' ? 'toEqual' : 'toBe'
         ](output); // if output is a complexe object use 'toEqual' otherwise 'toBe'
       });
     }
+  }
+
+  private checkInvokation(fn: Function) {
+    // Throw error if _classToInvoke doesn't describe functionToTest or if prototype of functionToTest & function identified with name in _classToInvoke
+    if (!this._classToInvoke[fn.name] || fn !== this._classToInvoke[fn.name])
+      throw new Error(
+        `${this._classToInvoke.name}.${fn.name} is not the expected tested one`,
+      );
   }
 }
