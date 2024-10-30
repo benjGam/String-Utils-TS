16ecbd6579976da63a37047302fb0b0c453daf48
benjGam <mecopilive@gmail.com>
2024-10-30 04:29:01 +0100

    fix: fix test

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -98,11 +98,11 @@ runner.runBasicTests(
 
 runner.runBasicTests(
   StringUtils.getFirstIndexOfUnfullyDeterminated,
-  new Map<[string, string], number>([
-    [['test', 'te'], 0],
-    [['', ''], -1],
-    [['hello', 'o'], 4],
-    [['hey ', ' '], 3],
-    [['bouh', 'hello'], 2],
+  new Map<any, number>([
+    [() => ['test', 'te'], 0],
+    [() => ['', ''], -1],
+    [() => ['hello', 'o'], 4],
+    [() => ['hey ', ' '], 3],
+    [() => ['bouh', 'hello'], 1],
   ]),
 );
